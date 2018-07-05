33fbcbb1d3e65b6f37a9bed3cf0724274b9200b8
Tommy Keswick <tkeswick@caltech.edu>
2018-07-05 15:47:27 -0700

    Add creation of new PIDs file.

--- scripts/modify-mods.php
+++ scripts/modify-mods.php
@@ -1,10 +1,16 @@
 <?php
 
-// script purpose: load each MODS file and rewrite the title line
-// assumes book and page MODS files were generated with
-// `drush islandora_datastream_crud_fetch_datastreams` and are all located in
-// /path/to/source/files directory
-
+// script purposes:
+// - load each page MODS file and rewrite the title line
+// - add PIDs of rewritten files to /path/to/destination/files/pids file
+//
+// assumptions:
+// - book and page MODS files were generated with
+//   `drush islandora_datastream_crud_fetch_datastreams` and are all located in
+//   /path/to/source/files directory
+// - the namespace of each page is 'hale'
+// - each page sequence number has four digits, like '0123'
+//
 // run like:
 // php /path/to/modify-mods.php /path/to/source/files /path/to/destination/files
 
@@ -17,17 +23,27 @@ else {
   exit("\n🚫  \e[1;91mSTOP!\e[0m The source and destination directories must be supplied.\nExample: php /path/to/modify-mods.php /path/to/source/files /path/to/destination/files\n\n");
 }
 
-// http://php.net/manual/en/class.directoryiterator.php
+// recreate the PIDs file every time because we are appending to it
+$page_pids_file = $path_destination . '/page_pids';
+if (file_exists($page_pids_file)) {
+  unlink($page_pids_file);
+}
+
+// see: http://php.net/manual/en/class.directoryiterator.php
 $dirItem = new DirectoryIterator($path_source);
 foreach ($dirItem as $fileInfo) {
     if ((!$fileInfo->isDot()) && ($fileInfo->getExtension() == 'xml')) {
         $filename = $fileInfo->getFilename();
 
+        // parse $filename to get PID
+        $filename_parts = explode('_', $filename);
+        $pid = $filename_parts[0] . ':' . $filename_parts[1];
+
         // load MODS file
         $mods = simplexml_load_file($path_source . '/' . $filename);
 
         // note: page MODS files have 'mods' namespace
-        // http://us1.php.net/manual/en/simplexmlelement.children.php
+        // see: http://us1.php.net/manual/en/simplexmlelement.children.php
         $title = $mods->children('mods', TRUE)->titleInfo->title;
 
         if (strpos($title, ', page 0') > 0) {
@@ -39,8 +55,12 @@ foreach ($dirItem as $fileInfo) {
             $title_new = substr($title, 0, -4) . $page_number;
             // set new title in $mods object
             $mods->children('mods', TRUE)->titleInfo->title = $title_new;
-            // http://us1.php.net/manual/en/simplexmlelement.asxml.php
+            // see: http://us1.php.net/manual/en/simplexmlelement.asxml.php
             $mods->asXML($path_destination . '/' . $filename);
+            // write PID to $page_pids_file
+            $append_handle = fopen($page_pids_file, 'a') or die('cannot open file: ' . $page_pids_file);
+            fwrite($append_handle, "$pid\n");
+            fclose($append_handle);
             echo "✅  wrote $title_new to $filename\n";
         }
         elseif (strpos($title, ', page 0') === 0) {
@@ -62,8 +82,12 @@ foreach ($dirItem as $fileInfo) {
             $title_new = $book_title . ', page ' . $page_number;
             // set new title in $mods object
             $mods->children('mods', TRUE)->titleInfo->title = $title_new;
-            // http://us1.php.net/manual/en/simplexmlelement.asxml.php
+            // see: http://us1.php.net/manual/en/simplexmlelement.asxml.php
             $mods->asXML($path_destination . '/' . $filename);
+            // write PID to $page_pids_file
+            $append_handle = fopen($page_pids_file, 'a') or die('cannot open file: ' . $page_pids_file);
+            fwrite($append_handle, "$pid\n");
+            fclose($append_handle);
             echo "✅  wrote $title_new to $filename\n";
         }
         else {
